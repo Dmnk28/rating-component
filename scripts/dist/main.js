1cb152a62ee40827abddce2674c226b10ce2b795
Dominik Oesterle <dominik.oesterle@posteo.de>
2022-05-09 11:39:49 +0200

    Completed the Project

--- scripts/dist/main.js
+++ scripts/dist/main.js
@@ -1,31 +1,52 @@
 "use strict";
-const switchCardState = () => {
-    /* Get Card */
-    const card = document.getElementById('card');
-    /* Get Card-Content */
-    const icon = document.getElementById('icon-img');
-    const heading = document.getElementById('card-heading');
-    const text = document.getElementById('card-text');
-    card.classList.add('centered');
-    icon.src = './images/illustration-thank-you.svg';
-    heading.innerText = 'Thank you!';
-    text.innerText = 'We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!';
-};
-const handleSubmit = (event, form) => {
-    event.preventDefault();
-    form.style.display = "none";
-    switchCardState();
-};
 const cardControls = () => {
     /* Get Card-Form-Elements */
     const cardForm = document.getElementById('rating-ui');
     const submit = document.getElementById('submit-btn');
-    const ratingBtns = document.getElementsByClassName('star-btn');
-    let rating;
-    const handleRating = (event, target, otherBtns) => {
-        event.preventDefault();
+    const ratingBtns = Array.from(document.getElementsByClassName('star-btn'));
+    /* Get Card */
+    const card = document.getElementById('card');
+    /* Get Card-Content */
+    const icon = document.getElementById('icon');
+    const heading = document.getElementById('card-heading');
+    const text = document.getElementById('card-text');
+    /* Chip-Element for Thank-You-Page */
+    const ratingChip = document.createElement('div');
+    ratingChip.id = 'rating-chip';
+    ratingChip.classList.add('chip');
+    /* Image-Element for Thank-You-Page */
+    const image = document.createElement('img');
+    image.src = "./images/illustration-thank-you.svg";
+    image.classList.add("thank-you-image");
+    let rating; // Rating Value: Used number instead of string here, because in a real scenario the number would be more interessting for marketing departments, etc. For this simple project itself a string would elsewise be suitable. 
+    const handleRating = (event, target) => {
         rating = Number(target.value);
+        ratingBtns.forEach(btn => {
+            btn.classList.remove('selected');
+        });
         target.classList.add('selected');
+    };
+    ratingBtns.map(btn => {
+        btn.addEventListener("click", (event) => {
+            event.preventDefault();
+            handleRating(event, event.target);
+        }, false);
+    });
+    const switchCardState = () => {
+        card.classList.add('centered');
+        icon.remove();
+        ratingChip.innerText = `You selected ${rating.toString()} out of 5`;
+        card.prepend(ratingChip);
+        card.prepend(image);
+        heading.innerText = 'Thank you!';
+        text.innerText = 'We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!';
+    };
+    const handleSubmit = (event, form) => {
+        event.preventDefault();
+        if (!rating)
+            return alert('Please select a number between 1 and 5. Thanks!');
+        form.style.display = "none";
+        switchCardState();
     };
     submit === null || submit === void 0 ? void 0 : submit.addEventListener("click", (event) => {
         handleSubmit(event, cardForm);
--- scripts/src/main.ts
+++ scripts/src/main.ts
@@ -1,39 +1,62 @@
-const switchCardState = (): void => {
+const cardControls = (): void => {
+    /* Get Card-Form-Elements */
+    const cardForm: HTMLFormElement = document.getElementById('rating-ui') as HTMLFormElement;
+    const submit: HTMLButtonElement = document.getElementById('submit-btn') as HTMLButtonElement;
+    const ratingBtns: Element[]     = Array.from(document.getElementsByClassName('star-btn'));
     /* Get Card */
     const card: HTMLDivElement = document.getElementById('card') as HTMLDivElement;
     /* Get Card-Content */
-    const icon: HTMLImageElement        = document.getElementById('icon-img') as HTMLImageElement;
+    const icon: HTMLDivElement        = document.getElementById('icon') as HTMLDivElement;
     const heading: HTMLHeadingElement   = document.getElementById('card-heading') as HTMLHeadingElement;
     const text: HTMLParagraphElement    = document.getElementById('card-text') as HTMLParagraphElement;
-
-
-    card.classList.add('centered')
-    icon.src = './images/illustration-thank-you.svg';
-    heading.innerText = 'Thank you!';
-    text.innerText = 'We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!'
-}
-
-
-const handleSubmit = (event: Event, form: HTMLFormElement): void => {
-    event.preventDefault();
-    form.style.display = "none";
-    switchCardState();
-}
-
-const cardControls = (): void => {
-    /* Get Card-Form-Elements */
-    const cardForm: HTMLFormElement             = document.getElementById('rating-ui') as HTMLFormElement;
-    const submit: HTMLButtonElement             = document.getElementById('submit-btn') as HTMLButtonElement;
-    const ratingBtns: HTMLCollectionOf<Element> = document.getElementsByClassName('star-btn');
     
-    let rating: number;
+    /* Chip-Element for Thank-You-Page */
+    const ratingChip: HTMLDivElement    = document.createElement('div');
+    ratingChip.id = 'rating-chip';
+    ratingChip.classList.add('chip');
     
-    const handleRating = (event: Event, target: HTMLButtonElement, otherBtns: HTMLCollectionOf<Element>): void => {
-        event.preventDefault();
+    /* Image-Element for Thank-You-Page */
+    const image: HTMLImageElement = document.createElement('img');
+    image.src = "./images/illustration-thank-you.svg";
+    image.alt = "Thank you Illustration"
+    image.classList.add("thank-you-image")
+
+
+    let rating: number;         // Rating Value: Used number instead of string here, because in a real scenario the number would be more interessting for marketing departments, etc. For this simple project itself a string would elsewise be suitable. 
+    
+    const handleRating = (event: Event, target: HTMLButtonElement): void => {
         rating = Number(target.value);
+        ratingBtns.forEach(btn => {
+            btn.classList.remove('selected')
+        });
         target.classList.add('selected');
     }
-        
+    
+    ratingBtns.map(btn => {
+        btn.addEventListener("click", (event) => {
+            event.preventDefault();
+            handleRating(event, event.target as HTMLButtonElement)  
+        }, false)
+    })
+
+    const switchCardState = (): void => {
+        card.classList.add('centered')
+        icon.remove();
+        ratingChip.innerText = `You selected ${rating.toString()} out of 5`;
+        card.prepend(ratingChip);
+        card.prepend(image);
+        heading.innerText = 'Thank you!';
+        text.innerText = 'We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!'
+    }
+    
+    
+    const handleSubmit = (event: Event, form: HTMLFormElement): void => {
+        event.preventDefault();
+        if (!rating) return alert('Please select a number between 1 and 5. Thanks!')
+        form.style.display = "none";
+        switchCardState();
+    }
+
     submit?.addEventListener("click", (event) => {
         handleSubmit(event, cardForm)
     }, false);
